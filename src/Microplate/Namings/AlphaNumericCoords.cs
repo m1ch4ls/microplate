a81f9f8d56b83162c9b6c633f5ac31bfc368082e
Michal Sedlák <sedlakmichal@gmail.com>
2012-10-10 00:03:28 +0200

    Add postion naming stubs.

---
+++ src/Microplate/Namings/AlphaNumericCoords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Microplate.Namings
+{
+    public class AlphaNumericCoords : IPositionNaming
+    {
+        /// <summary>
+        /// GetPosition position name for given coordinates.
+        /// </summary>
+        public string GetRowName(int row)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convert position name to coordinates.
+        /// </summary>
+        public Point GetCoords(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetColName(int col)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Microplate/PositionNamings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Microplate.Namings;
+
+namespace Microplate
+{
+    public static class PositionNamings
+    {
+        public static readonly IPositionNaming NumericCoords = new NumericCoords();
+
+        public static readonly IPositionNaming AlphaNumericCoords = new AlphaNumericCoords();
+
+        public static readonly IPositionNaming NumericDirect = new NumericDirect();
+
+        public static readonly IPositionNaming AlphaNumericDirect = new AlphaNumericDirect();
+
+        public static IPositionNaming Default { get { return NumericCoords; } }
+    }
+}
