da9c35d64a0ebafaecd27891aa262ae873239ecb
Michal Sedlák <sedlakmichal@gmail.com>
2012-10-09 23:12:48 +0200

    Default IPlateType implementation + forgotten StateRecord file.

---
+++ src/Microplate/StateRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microplate
+{
+    public class StateRecord
+    {
+        public string State { get; set; }
+
+        public string Location { get; set; }
+    }
+}
---
+++ src/Microplate/Types/DefaultType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microplate.Types
+{
+    public class DefaultType : IPlateType
+    {
+        public string Name { get; set; }
+        public Format Format { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
