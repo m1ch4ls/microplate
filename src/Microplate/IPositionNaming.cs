af3c974b0bb3eb49e3c8f5461b3212a1552f3b7f
Michal Sedlák <sedlakmichal@gmail.com>
2012-10-10 01:02:17 +0200

    Add comments.

--- src/Microplate/IPositionNaming.cs
+++ src/Microplate/IPositionNaming.cs
@@ -6,19 +6,34 @@ using System.Drawing;
 
 namespace Microplate
 {
+    /// <summary>
+    /// We have 4 main position naming conventions. This is common interface.
+    /// </summary>
     public interface IPositionNaming
     {
-
         /// <summary>
-        /// GetPosition position name for given coordinates.
+        /// Row name for given position naming.
         /// </summary>
+        /// <param name="row">Internal row index starting with 0.</param>
+        /// <returns>
+        /// Row name string.
+        /// </returns>
         string GetRowName(int row);
 
         /// <summary>
         /// Convert position name to coordinates.
         /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns><see cref="Point"/> of given coordinates</returns>
         Point GetCoords(string name);
 
+        /// <summary>
+        /// Column name for given position naming.
+        /// </summary>
+        /// <param name="col">Internal column index starting with 0.</param>
+        /// <returns>
+        /// Column name string
+        /// </returns>
         string GetColName(int col);
     }
 }
