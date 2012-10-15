e355919414a7d51d1133c69b4ebadfbd0c2a5ace
Michal Sedlák <sedlakmichal@gmail.com>
2012-10-15 22:47:51 +0200

    Make all IData types Serializable

--- src/Microplate/Data/RT.cs
+++ src/Microplate/Data/RT.cs
@@ -1,8 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Runtime.Serialization;
 using System.Windows.Forms;
 
 namespace Microplate.Data
 {
+    [Serializable]
     public class RT : IData
     {
         public Image ToImage()
@@ -11,6 +14,27 @@ namespace Microplate.Data
         }
 
         public Control ToControl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext"/>) for this serialization.</param>
+        /// <exception cref="T:System.Security.SecurityException">The caller does not have the required permission.</exception>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RT"/> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> from which to create a new instance.</param>
+        /// <param name="context">The source (see <see cref="T:System.Runtime.Serialization.StreamingContext"/>) for this serialization.</param>
+        public RT(SerializationInfo info, StreamingContext context)
         {
             throw new System.NotImplementedException();
         }
--- src/Microplate/IData.cs
+++ src/Microplate/IData.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Windows.Forms;
 
 namespace Microplate
 {
-    public interface IData
+    public interface IData : ISerializable
     {
         string ToString();
 
