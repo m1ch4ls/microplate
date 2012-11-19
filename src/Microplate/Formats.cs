7d3b38554f0e0159e17824162f32a034bd0a65ce
Michal Sedlák <sedlakmichal@gmail.com>
2012-11-19 12:37:54 +0100

    Add default format for testing and such

--- src/Microplate/Formats.cs
+++ src/Microplate/Formats.cs
@@ -20,9 +20,22 @@ namespace Microplate
                                                                   {"96-well", new Format(8, 12, PositionNamings.Default, "96-well")},
                                                                   {"384-well", new Format(16, 24, PositionNamings.Default, "384-well")},
                                                               };
+        /// <summary>
+        /// Not a plate but a queue of tubes
+        /// </summary>
+        /// <param name="n">The number of tubes.</param>
+        /// <returns></returns>
         public static Format SingleTubes(int n)
         {
             return new Format(1, n, PositionNamings.Default, String.Format("Single tubes ({0})", n));
+        }
+
+        /// <summary>
+        /// Gets the default 4x4 format for testing.
+        /// </summary>
+        public static Format Default
+        {
+            get { return new Format(4, 4, PositionNamings.Default); }
         }
     }
 }
